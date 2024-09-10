0280f4e35180041ff268a5c18e50d47578265674
Armond-Guze <Armond.Guze@yahoo.com>
2024-09-10 00:40:17 -0400

    sdfs

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,26 +1,66 @@
-import React from 'react'
-
-const movies = [
-    { id: 1, title: 'Inception', year: 2010, rating: 'PG-13' },
-    { id: 2, title: 'The Dark Knight', year: 2008, rating: 'PG-13' },
-    { id: 3, title: 'Interstellar', year: 2014, rating: 'PG-13' },
-    { id: 4, title: 'The Matrix', year: 1999, rating: 'R' }
-  ];
-
-  const handleAlert ()
+import React from "react";
+import { useState } from "react";
 
 const Headers = () => {
-  return (
-    <div>
-            {movies.map((movie) => (
-                <div key={movie.id}>
-                    {movie.title} - {movie.rating} - {movie.year}
-                    <button>Click me</button>
-                </div>
-            ))}
-        
-    </div>
-  )
-}
+  const [tasks, setTask] = useState([
+    { name: "Walk the dog", completed: true },
+  ]);
+  const [newTask, setNewtask] = useState("");
 
-export default Headers
+  const toggleTask = (index) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, completed: !task.completed } : task
+    );
+    setTask(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+  };
+
+  const createNewTask = () => {
+    if (newTask !== "") {
+      const updatedTasks = [...tasks, { name: newTask, completed: false }];
+      setTask(updatedTasks);
+      setNewtask("");
+    }
+  };
+
+  return (
+    <div className="justify-center flex flex-col items-center">
+      <h1 className="text-[50px] font-semibold">ToDo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewtask(e.target.value)}
+          placeholder="Add new task.."
+          className="bg-black rounded p-2 text-white"
+        />
+        <button
+          className="bg-black text-white p-2 ml-2 rounded"
+          onClick={createNewTask}
+        >
+          NewTask
+        </button>
+      </div>
+
+      {tasks.map((task, index) => (
+        <ul key={index} className="flex items-center mb-2">
+          <div className="flex items-center mt-4 space-x-2">
+            {task.name}
+            <button className={`btn ml-2 ${task.completed ? 'bg-green-500' : 'bg-red-500'}`} onClick={() => toggleTask(index)}>
+              {task.completed ? "Complete" : "Incomplete"}
+            </button>
+            <button className="btn ml-2" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </div>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default Headers;

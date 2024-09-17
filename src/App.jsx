a4c00a8a1027af3b7e84f7e1cc99c10afca47230
Armond-Guze <Armond.Guze@yahoo.com>
2024-09-17 07:32:43 -0400

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Headers from "./components/Headers";
 const App = () => {
   return (
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { useState } from "react"
 
 const Headers = () => {
+  const [tasks, setTasks] = useState([])
+  
   return (
     <div>Headers</div>
   )
 }
-// df
 
 export default Headers
